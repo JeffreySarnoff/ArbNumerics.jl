a8e107f505e684d57d9d58600b9070ea864fee45
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-05-29 18:35:41 -0400

    Update eps_ulp.jl

--- src/intervals/eps_ulp.jl
+++ src/intervals/eps_ulp.jl
@@ -1,3 +1,14 @@
+#=
+    significand_bits
+
+    Returns the number of bits needed to represent
+    the absolute value of the significand of an
+    Arf{P} value, directly or as an Arb{P} midpoint.
+    This is the minimum precision in bits that
+    allows an exact representation of that value.
+    Returns 0 if x is a special value.
+=#
+
 function significand_bits(x::Arf{P}) where {P}
     # returns the number of bits needed to represent the absolute value of the significand of x,
     # i.e. the minimum precision sufficient to represent x exactly. Returns 0 if x is a special value.
@@ -37,6 +48,8 @@ end
     and whose radius is the larger out of the real and imaginary radiuses of x.
 =#
 
+relerror_bits(x::Arf{P}) = 0
+
 function relerror_bits(x::Arb{P}) where {P}
     nbits = ccall(@libarb(arb_rel_error_bits), Clong, (Ref{Arb},), x)
     return nbits
@@ -46,6 +59,55 @@ function relerror_bits(x::Acb{P}) where {P}
     nbits = ccall(@libarb(crb_rel_error_bits), Clong, (Ref{Acb},), x)
     return nbits
 end
+
+#=
+    rel_accuracy_bits
+
+    Returns the effective relative accuracy intrinsic to a ball, measured in bits.
+    This is equal to -relerror_bits.
+=#
+
+
+relaccuracy_bits(x::Arf{P}) where {P} = 0
+
+function relaccuracy_bits(x::Arb{P}) where {P}
+    nbits = ccall(@libarb(arb_rel_accuracy_bits), Clong, (Ref{Arb},), x)
+    return nbits
+end
+
+function relaccuracy_bits(x::Acb{P}) where {P}
+    nbits = ccall(@libarb(acb_rel_accuracy_bits), Clong, (Ref{Acb},), x)
+    return nbits
+end
+
+
+#=
+    Rounds the significand of a value to the number of bits
+    equal to the accuracy of x (as indicated by its radius),
+    plus a few guard bits so enclosure is maintained.
+=#
+
+trim_bits(x::Mag) = x
+
+function trim_bits(x::Arf{P}, roundingmode::RoundingMode=RoundFromZero) where {P}
+    nbits = significand_bits(x)
+    nbits == P && return x
+    rounding = match_rounding_mode(roundingmode)
+    z = Arf{nbits}
+    res = ccall(@libarb(arf_set_round), Cint, (Ref{Arf}, Ref{Arf}, Clong, Cint), z, x, nbits, rounding)
+    return res
+end
+function trim_bits(x::Arb{P}) where {P}
+    z = Arb{P}()
+    ccall(@libarb(arb_trim), Cvoid, (Ref{Arb}, Ref{Arb}), z, x)
+    return z
+end
+function trim_bits(x::Acb{P}) where {P}
+    z = Acb{P}()
+    ccall(@libarb(acb_trim), Cvoid, (Ref{Acb}, Ref{Acb}), z, x)
+    return z
+end
+
 
 
 function ulp(x::Arf{P}) where {P}
