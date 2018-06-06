b1d3735373409c482f93d3e93e0c71a832f1c939
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-05 22:20:07 -0400

    Add files via upload

--- src/intervals/eps_ulp.jl
+++ src/intervals/eps_ulp.jl
@@ -3,37 +3,37 @@
 
     Returns the number of bits needed to represent
     the absolute value of the significand of an
-    Arf{P} value, directly or as an Arb{P} midpoint.
+    ArbFloat{P} value, directly or as an ArbBall{P} midpoint.
     This is the minimum precision in bits that
     allows an exact representation of that value.
     Returns 0 if x is a special value.
 =#
 
-function significand_bits(x::Arf{P}) where {P}
+function significand_bits(x::ArbFloat{P}) where {P}
     # returns the number of bits needed to represent the absolute value of the significand of x,
     # i.e. the minimum precision sufficient to represent x exactly. Returns 0 if x is a special value.
-    nbits = ccall(@libarb(arf_bits), Clong, (Ref{Arf},), x)
+    nbits = ccall(@libarb(arf_bits), Clong, (Ref{ArbFloat},), x)
     return nbits
 end
 
-function significand_bits(x::Arb{P}) where {P}
+function significand_bits(x::ArbBall{P}) where {P}
     # returns the number of bits needed to represent the absolute value of the significand of midpoint(x),
     # i.e. the minimum precision sufficient to represent x exactly. Returns 0 if x is a special value.
-    nbits = ccall(@libarb(arb_bits), Clong, (Ref{Arb},), x)
+    nbits = ccall(@libarb(arb_bits), Clong, (Ref{ArbBall},), x)
     return nbits
 end
 
-function significand_bits(x::Acb{P}) where {P}
+function significand_bits(x::ArbComplex{P}) where {P}
     # returns the number of bits needed to represent the maximum of bits(real(x)), bits(imag(x)).
-    nbits = ccall(@libarb(acb_bits), Clong, (Ref{Acb},), x)
+    nbits = ccall(@libarb(acb_bits), Clong, (Ref{ArbComplex},), x)
     return nbits
 end
 
 
-#= 
+#=
     arb_rel_error_bits
-    
-    Returns the effective relative error of x measured in bits, 
+
+    Returns the effective relative error of x measured in bits,
     defined as the difference between
     the position of the top bit in the radius
     and the top bit in the midpoint, plus one.
@@ -41,22 +41,22 @@ end
     (ARF_PREC_EXACT == ±(2^{63}-1) inclusive on a 64-bit machine).
 
     acb_rel_error_bits
-    
+
     Returns the effective relative error of x measured in bits.
     This is computed as if calling arb_rel_error_bits() on the real ball
     whose midpoint is the larger out of the real and imaginary midpoints of x,
     and whose radius is the larger out of the real and imaginary radiuses of x.
 =#
 
-relerror_bits(x::Arf{P}) where {P} = 0
+relerror_bits(x::ArbFloat{P}) where {P} = 0
 
-function relerror_bits(x::Arb{P}) where {P}
-    nbits = ccall(@libarb(arb_rel_error_bits), Clong, (Ref{Arb},), x)
+function relerror_bits(x::ArbBall{P}) where {P}
+    nbits = ccall(@libarb(arb_rel_error_bits), Clong, (Ref{ArbBall},), x)
     return nbits
 end
 
-function relerror_bits(x::Acb{P}) where {P}
-    nbits = ccall(@libarb(crb_rel_error_bits), Clong, (Ref{Acb},), x)
+function relerror_bits(x::ArbComplex{P}) where {P}
+    nbits = ccall(@libarb(crb_rel_error_bits), Clong, (Ref{ArbComplex},), x)
     return nbits
 end
 
@@ -68,15 +68,15 @@ end
 =#
 
 
-relaccuracy_bits(x::Arf{P}) where {P} = 0
+relaccuracy_bits(x::ArbFloat{P}) where {P} = 0
 
-function relaccuracy_bits(x::Arb{P}) where {P}
-    nbits = ccall(@libarb(arb_rel_accuracy_bits), Clong, (Ref{Arb},), x)
+function relaccuracy_bits(x::ArbBall{P}) where {P}
+    nbits = ccall(@libarb(arb_rel_accuracy_bits), Clong, (Ref{ArbBall},), x)
     return nbits
 end
 
-function relaccuracy_bits(x::Acb{P}) where {P}
-    nbits = ccall(@libarb(acb_rel_accuracy_bits), Clong, (Ref{Acb},), x)
+function relaccuracy_bits(x::ArbComplex{P}) where {P}
+    nbits = ccall(@libarb(acb_rel_accuracy_bits), Clong, (Ref{ArbComplex},), x)
     return nbits
 end
 
@@ -89,68 +89,68 @@ end
 
 trim_bits(x::Mag) = x
 
-function trim_bits(x::Arf{P}, roundingmode::RoundingMode=RoundFromZero) where {P}
+function trim_bits(x::ArbFloat{P}, roundingmode::RoundingMode=RoundFromZero) where {P}
     nbits = significand_bits(x)
     nbits == P && return x
     rounding = match_rounding_mode(roundingmode)
-    z = Arf{nbits}
-    res = ccall(@libarb(arf_set_round), Cint, (Ref{Arf}, Ref{Arf}, Clong, Cint), z, x, nbits, rounding)
+    z = ArbFloat{nbits}
+    res = ccall(@libarb(arf_set_round), Cint, (Ref{ArbFloat}, Ref{ArbFloat}, Clong, Cint), z, x, nbits, rounding)
     return res
 end
-function trim_bits(x::Arb{P}) where {P}
-    z = Arb{P}()
-    ccall(@libarb(arb_trim), Cvoid, (Ref{Arb}, Ref{Arb}), z, x)
+function trim_bits(x::ArbBall{P}) where {P}
+    z = ArbBall{P}()
+    ccall(@libarb(arb_trim), Cvoid, (Ref{ArbBall}, Ref{ArbBall}), z, x)
     return z
 end
-function trim_bits(x::Acb{P}) where {P}
-    z = Acb{P}()
-    ccall(@libarb(acb_trim), Cvoid, (Ref{Acb}, Ref{Acb}), z, x)
+function trim_bits(x::ArbComplex{P}) where {P}
+    z = ArbComplex{P}()
+    ccall(@libarb(acb_trim), Cvoid, (Ref{ArbComplex}, Ref{ArbComplex}), z, x)
     return z
 end
 
 
 
-function ulp(x::Arf{P}) where {P}
+function ulp(x::ArbFloat{P}) where {P}
     w = Mag()
     #   Sets z to the magnitude of the unit in the last place (ulp) of x at precision P.
-    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{Arf}, Clong), w, x, P)
-    z = Arf{P}(w)
+    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{ArbFloat}, Clong), w, x, P)
+    z = ArbFloat{P}(w)
     return z
 end
 
-function eps(x::Arf{P}) where {P}
+function eps(x::ArbFloat{P}) where {P}
     w = Mag()
     #   Sets z to twice the magnitude of the unit in the last place (ulp) of x at precision P.
-    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{Arf}, Clong), w, x, P)
-    z = Arf{P}(w)
+    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{ArbFloat}, Clong), w, x, P)
+    z = ArbFloat{P}(w)
     z += z
     return z
 end
 
-function ulp(x::Arf{P}, prec::Int) where {P}
+function ulp(x::ArbFloat{P}, prec::Int) where {P}
     prec < MINIMUM_PRECISION && throw(DomainError("bit precision ($prec) is too low"))
     w = Mag()
     #   Sets z to the magnitude of the unit in the last place (ulp) of x at precision prec.
-    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{Arf}, Clong), w, x, prec)
-    z = Arf{P}(w)
+    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{ArbFloat}, Clong), w, x, prec)
+    z = ArbFloat{P}(w)
     return z
 end
 
-function eps(x::Arf{P}, prec::Int) where {P}
+function eps(x::ArbFloat{P}, prec::Int) where {P}
     prec < MINIMUM_PRECISION && throw(DomainError("bit precision ($prec) is too low"))
     w = Mag()
     #   Sets z to twice the magnitude of the unit in the last place (ulp) of x at precision prec.
-    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{Arf}, Clong), w, x, prec)
-    z = Arf{P}(w)
+    ccall(@libarb(arf_mag_set_ulp), Cvoid, (Ref{Mag}, Ref{ArbFloat}, Clong), w, x, prec)
+    z = ArbFloat{P}(w)
     z += z
     return z
 end
 
-ulp(x::Arb{P}) where {P} = ulp(midpoint_byref(x))
-eps(x::Arb{P}) where {P} = eps(midpoint_byref(x))
+ulp(x::ArbBall{P}) where {P} = ulp(midpoint_byref(x))
+eps(x::ArbBall{P}) where {P} = eps(midpoint_byref(x))
 
-ulp(x::Acb{P}) where {P} = ulp(midpoint_byref(real(x)))
-eps(x::Acb{P}) where {P} = eps(midpoint_byref(real(x)))
+ulp(x::ArbComplex{P}) where {P} = ulp(midpoint_byref(real(x)))
+eps(x::ArbComplex{P}) where {P} = eps(midpoint_byref(real(x)))
 
-ulps(x::Acb{P}) where {P} = ulp(midpoint_byref(real(x))), ulp(midpoint_byref(imag(x)))
-epss(x::Acb{P}) where {P} = eps(midpoint_byref(real(x))), eps(midpoint_byref(imag(x)))
+ulps(x::ArbComplex{P}) where {P} = ulp(midpoint_byref(real(x))), ulp(midpoint_byref(imag(x)))
+epss(x::ArbComplex{P}) where {P} = eps(midpoint_byref(real(x))), eps(midpoint_byref(imag(x)))
--- src/intervals/intervalparts.jl
+++ src/intervals/intervalparts.jl
@@ -1,142 +1,142 @@
-function setinterval(lo::Arf{P}, hi::Arf{P}) where {P}
+function setinterval(lo::ArbFloat{P}, hi::ArbFloat{P}) where {P}
     lo > hi && return setinterval(hi, lo)
-    z = Arb{P}()
-    ccall(@libarb(arb_set_interval_arf), Cvoid, (Ref{Arb}, Ref{Arf}, Ref{Arf}, Clong), z, lo, hi, P)
+    z = ArbBall{P}()
+    ccall(@libarb(arb_set_interval_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}, Ref{ArbFloat}, Clong), z, lo, hi, P)
     return z
 end
 
-function setinterval(lo::Arb{P}, hi::Arb{P}) where {P}
+function setinterval(lo::ArbBall{P}, hi::ArbBall{P}) where {P}
     lo > hi && return setinterval(hi, lo)
-    setinterval(lowerbound(lo, Arf), upperbound(hi, Arf))
+    setinterval(lowerbound(lo, ArbFloat), upperbound(hi, ArbFloat))
 end
 
-function getinterval(x::Arb{P}, ::Type{Arf}) where {P}
-    Arf{P}(lowerbound(x)), Arf{P}(upperbound(x))
+function getinterval(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    ArbFloat{P}(lowerbound(x)), ArbFloat{P}(upperbound(x))
 end
 
-function getinterval(x::Arb{P}) where {P}
+function getinterval(x::ArbBall{P}) where {P}
    lowerbound(x), upperbound(x)
 end
 
-function setball(mid::Arf{P}, rad::Arf{P}) where {P}
+function setball(mid::ArbFloat{P}, rad::ArbFloat{P}) where {P}
     signbit(rad) && throw(ErrorException("nonnegative radius required ($rad)"))
     lbound = mid - rad
     ubound = mid + rad
     setinterval(lbound, ubound)
 end
 
-function setball(mid::Arb{P}, rad::Arb{P}) where {P}
-    signbit(rad) && throw(ErrorException("nonnegative radius required ($rad)"))    
-    setball(Arf{P}(mid), Arf{P}(rad))
+function setball(mid::ArbBall{P}, rad::ArbBall{P}) where {P}
+    signbit(rad) && throw(ErrorException("nonnegative radius required ($rad)"))
+    setball(ArbFloat{P}(mid), ArbFloat{P}(rad))
 end
 
-function getball(x::Arb{P}, ::Type{Arf}) where {P}
-    Arf{P}(midpoint(x)), Arf{P}(radius(x))
+function getball(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    ArbFloat{P}(midpoint(x)), ArbFloat{P}(radius(x))
 end
 
-function getball(x::Arb{P}) where {P}
+function getball(x::ArbBall{P}) where {P}
     midpoint(x), radius(x)
 end
 
-function getball(x::Arf{P}) where {P}
+function getball(x::ArbFloat{P}) where {P}
     x, zero(typeof(x))
 end
 
 
-@inline function upperbound(x::Arb{P}, ::Type{Arf}) where {P}
-    w = Arf{P}()  
-    ccall(@libarb(arb_get_ubound_arf), Cvoid, (Ref{Arf}, Ref{Arb}, Clong), w, x, P)
+@inline function upperbound(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    w = ArbFloat{P}()
+    ccall(@libarb(arb_get_ubound_arf), Cvoid, (Ref{ArbFloat}, Ref{ArbBall}, Clong), w, x, P)
     return w
 end
 
-function upperbound(x::Arb{P}) where {P}
-    w = upperbound(x, Arf)
-    z = Arb{P}()
-    ccall(@libarb(arb_set_arf), Cvoid, (Ref{Arb}, Ref{Arf}), z, w)
+function upperbound(x::ArbBall{P}) where {P}
+    w = upperbound(x, ArbFloat)
+    z = ArbBall{P}()
+    ccall(@libarb(arb_set_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), z, w)
     return z
 end
 
-@inline function lowerbound(x::Arb{P}, ::Type{Arf}) where {P}
-    w = Arf{P}()  
-    ccall(@libarb(arb_get_lbound_arf), Cvoid, (Ref{Arf}, Ref{Arb}, Clong), w, x, P)
+@inline function lowerbound(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    w = ArbFloat{P}()
+    ccall(@libarb(arb_get_lbound_arf), Cvoid, (Ref{ArbFloat}, Ref{ArbBall}, Clong), w, x, P)
     return w
 end
 
-function lowerbound(x::Arb{P}) where {P}
-    w = lowerbound(x, Arf)
-    z = Arb{P}()
-    ccall(@libarb(arb_set_arf), Cvoid, (Ref{Arb}, Ref{Arf}), z, w)
+function lowerbound(x::ArbBall{P}) where {P}
+    w = lowerbound(x, ArbFloat)
+    z = ArbBall{P}()
+    ccall(@libarb(arb_set_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), z, w)
     return z
 end
 
-function bounds(x::Arb{P}, ::Type{Arf}) where {P}
-    lowerbound(x, Arf), upperbound(x, Arf)
+function bounds(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    lowerbound(x, ArbFloat), upperbound(x, ArbFloat)
 end
 
-function bounds(x::Arb{P}) where {P}
+function bounds(x::ArbBall{P}) where {P}
     lowerbound(x), upperbound(x)
 end
 
 
-@inline function upperbound_abs(x::Arb{P}, ::Type{Arf}) where {P}
-    w = Arf{P}()  
-    ccall(@libarb(arb_get_abs_ubound_arf), Cvoid, (Ref{Arf}, Ref{Arb}, Clong), w, x, P)
+@inline function upperbound_abs(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    w = ArbFloat{P}()
+    ccall(@libarb(arb_get_abs_ubound_arf), Cvoid, (Ref{ArbFloat}, Ref{ArbBall}, Clong), w, x, P)
     return w
 end
 
-function upperbound_abs(x::Arb{P}) where {P}
-    w = upperbound_abs(x, Arf)
-    z = Arb{P}()
-    ccall(@libarb(arb_set_arf), Cvoid, (Ref{Arb}, Ref{Arf}), z, w)
+function upperbound_abs(x::ArbBall{P}) where {P}
+    w = upperbound_abs(x, ArbFloat)
+    z = ArbBall{P}()
+    ccall(@libarb(arb_set_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), z, w)
     return z
 end
 
-@inline function lowerbound_abs(x::Arb{P}, ::Type{Arf}) where {P}
-    w = Arf{P}()  
-    ccall(@libarb(arb_get_abs_lbound_arf), Cvoid, (Ref{Arf}, Ref{Arb}, Clong), w, x, P)
+@inline function lowerbound_abs(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    w = ArbFloat{P}()
+    ccall(@libarb(arb_get_abs_lbound_arf), Cvoid, (Ref{ArbFloat}, Ref{ArbBall}, Clong), w, x, P)
     return w
 end
 
-function lowerbound_abs(x::Arb{P}) where {P}
-    w = lowerbound(x, Arf)
-    z = Arb{P}()
-    ccall(@libarb(arb_set_arf), Cvoid, (Ref{Arb}, Ref{Arf}), z, w)
+function lowerbound_abs(x::ArbBall{P}) where {P}
+    w = lowerbound(x, ArbFloat)
+    z = ArbBall{P}()
+    ccall(@libarb(arb_set_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), z, w)
     return z
 end
 
-function bounds_abs(x::Arb{P}, ::Type{Arf}) where {P}
-    lowerbound_abs(x, Arf), upperbound_abs(x, Arf)
+function bounds_abs(x::ArbBall{P}, ::Type{ArbFloat}) where {P}
+    lowerbound_abs(x, ArbFloat), upperbound_abs(x, ArbFloat)
 end
 
-function bounds_abs(x::Arb{P}) where {P}
+function bounds_abs(x::ArbBall{P}) where {P}
     lowerbound_abs(x), upperbound_abs(x)
 end
 
 
-function increase_radius(x::Arb{P}, err::Arf{P}) where {P}
+function increase_radius(x::ArbBall{P}, err::ArbFloat{P}) where {P}
     err >= 0 || throw(ErrorException("nonnegative err required ($err)"))
-    ccall(@libarb(arb_add_error_arf), Cvoid, (Ref{Arb}, Ref{Arf}), x, err)
+    ccall(@libarb(arb_add_error_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), x, err)
     return x
 end
 
-function increase_radius(x::Arb{P}, err::Arb{P}) where {P}
+function increase_radius(x::ArbBall{P}, err::ArbBall{P}) where {P}
     err >= 0 || throw(ErrorException("nonnegative err required ($err)"))
-    ccall(@libarb(arb_add_error_arb), Cvoid, (Ref{Arb}, Ref{Arb}), x, err)
+    ccall(@libarb(arb_add_error_arb), Cvoid, (Ref{ArbBall}, Ref{ArbBall}), x, err)
     return x
 end
 
-increase_radius(x::Arb{P}) where {P} = increase_radius(x, ulp(x))
+increase_radius(x::ArbBall{P}) where {P} = increase_radius(x, ulp(x))
 
-function decrease_radius(x::Arb{P}, err::Arf{P}) where {P}
+function decrease_radius(x::ArbBall{P}, err::ArbFloat{P}) where {P}
     err = -abs(err)
-    ccall(@libarb(arb_add_error_arf), Cvoid, (Ref{Arb}, Ref{Arf}), x, err)
+    ccall(@libarb(arb_add_error_arf), Cvoid, (Ref{ArbBall}, Ref{ArbFloat}), x, err)
     return x
 end
 
-function decrease_radius(x::Arb{P}, err::Arb{P}) where {P}
+function decrease_radius(x::ArbBall{P}, err::ArbBall{P}) where {P}
     err = -abs(err)
-    ccall(@libarb(arb_add_error_arb), Cvoid, (Ref{Arb}, Ref{Arb}), x, err)
+    ccall(@libarb(arb_add_error_arb), Cvoid, (Ref{ArbBall}, Ref{ArbBall}), x, err)
     return x
 end
 
-decrease_radius(x::Arb{P}) where {P} = decrease_radius(x, ulp(x))
+decrease_radius(x::ArbBall{P}) where {P} = decrease_radius(x, ulp(x))
