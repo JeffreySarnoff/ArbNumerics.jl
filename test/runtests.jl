f5c8eacd8ab77fd96b7307c2a1da97896261f656
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-06-27 16:46:19 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using ArbNumerics
 using Test
 
+import Base: parse
+
 setprecision(BigFloat, 512)
 
 DIGITS = 78;
@@ -26,7 +28,7 @@ bigfloat = BigFloat(float64)
 arbball  = ArbBall(float64, 64)
 
 for fn in (:sqrt, :log, :log1p, :exp, :expm1)
-    @eval v1 = Meta.parse(Float64, string($fn(bigfloat)))
-    @eval v2 = Meta.parse(Float64, string(midpoint($fn(arbball))))
+    @eval v1 = parse(Float64, string($fn(bigfloat)))
+    @eval v2 = parse(Float64, string(midpoint($fn(arbball))))
     @test v1 == v2
 end
