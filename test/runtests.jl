c1e6a8c67251b3b3f7d55b0123a20342e4afb4a2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-17 02:57:06 -0400

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,14 @@ setprecision(BigFloat, 512)
 
 DIGITS = 78;
 afpi = ArbFloat(pi, bits4digits(DIGITS))
-abpi = ArbBall(pi, bits4digits(DIGITS))
+abpi = ArbReal(pi, bits4digits(DIGITS))
 
 @test DIGITS == length(string(afpi)) - 1 # (-1 for the decimal point)
 @test DIGITS == length(string(abpi)) - 1 # (-1 for the decimal point)
 
 float64  = 0.125
 bigfloat = BigFloat(float64)
-arbball  = ArbBall(float64, 64)
+arbball  = ArbReal(float64, 64)
 
 for fn in (:sin, :cos, :tan, :csc, :sec, :cot,
            :asin, :acos, :atan)
@@ -25,7 +25,7 @@ end
 
 float64  = 1.125
 bigfloat = BigFloat(float64)
-arbball  = ArbBall(float64, 64)
+arbball  = ArbReal(float64, 64)
 
 for fn in (:sqrt, :log, :log1p, :exp, :expm1)
     @eval v1 = parse(Float64, string($fn(bigfloat)))
