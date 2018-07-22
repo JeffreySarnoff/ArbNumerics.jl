1b29831954a87f29ec457d92970e47e1d3f4b442
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-07-21 21:35:52 -0400

    Create helptext.jl

---
+++ src/support/helptext.jl
@@ -0,0 +1,38 @@
+"""
+   shi(x) 
+   
+   hyperbolic sine integral function 
+   
+   shi(x) = ₀∫ˣ (sinh(t) / t) δt 
+""" shi
+
+"""
+    chi(x)
+    
+    hyperbolic cosine integral
+    
+chi(x) = γ + ln(x) + ₀∫ˣ ((cosh(t) - 1) / t) δt 
+where γ == 0.5772_1566_4901_5328_6060_6512_0901-
+           (the Euler-Mascheroni constant)
+""" chi
+
+"""
+    li(x)
+   
+    logarithmic integral
+    
+li(x) =  ᵤ∫ˣ (δt / ln(t))
+
+         where u = 1.4513_6923_4883_3810_5028+
+                   (Soldner's constant)
+""" li
+
+"""
+    si(x)
+    
+    sine integral
+    
+si(x) = ₀∫ˣ (sin(t) / t) δt
+""" si
+
+
