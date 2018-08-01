f372a55a71dcaff02ba9a9f43aadca15d74eeb0a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-01 14:14:23 -0400

    Update show.jl

--- src/libarb/show.jl
+++ src/libarb/show.jl
@@ -3,20 +3,21 @@ function show(io::IO, x::Mag)
     print(io, str)
 end
 
-function show(io::IO, x::ArbFloat{P}, flags::Culong=NO_RADIUS) where {P}
+
+function show(io::IO, x::ArbFloat{P}; flags::Culong=NO_RADIUS) where {P}
     prec = digit_precision(P)
-    str = string(x, prec, flags)
+    str = string(x, prec, flags=flags)
     print(io, str)
 end
 
-function show(io::IO, x::ArbReal{P}, flags::Culong=NO_RADIUS) where {P}
+function show(io::IO, x::ArbReal{P}; flags::Culong=NO_RADIUS) where {P}
     prec = digit_precision(P)
-    str = string(x, prec, flags)
+    str = string(x, prec, flags=flags)
     print(io, str)
 end
 
-function show(io::IO, x::ArbComplex{P}, flags::Culong=NO_RADIUS) where {P}
+function show(io::IO, x::ArbComplex{P}; flags::Culong=NO_RADIUS) where {P}
     prec = digit_precision(P)
-    str = string(x, prec, flags)
+    str = string(x, prec, flags=flags)
     print(io, str)
 end
