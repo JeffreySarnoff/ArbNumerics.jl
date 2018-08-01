3fbd138522751db88df12d4587cfbeb65185bf12
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-01 15:06:00 -0400

    Update show.jl

--- src/libarb/show.jl
+++ src/libarb/show.jl
@@ -4,20 +4,23 @@ function show(io::IO, x::Mag)
 end
 
 
-function show(io::IO, x::ArbFloat{P}; flags::Culong=NO_RADIUS) where {P}
-    prec = digit_precision(P)
+function show(io::IO, x::ArbFloat{P}; midpoint::Bool=false, radius::Bool=false) where {P}
+    prec = midpoint ? digits4bits(P) : digit_precision(P)
+    flags = radius ? ARB_STR_NO_RADIUS : ARB_STR_RADIUS
     str = string(x, prec, flags=flags)
     print(io, str)
 end
 
-function show(io::IO, x::ArbReal{P}; flags::Culong=NO_RADIUS) where {P}
-    prec = digit_precision(P)
+function show(io::IO, x::ArbReal{P}; midpoint::Bool=false, radius::Bool=false) where {P}
+    prec = midpoint ? digits4bits(P) : digit_precision(P)
+    flags = radius ? ARB_STR_NO_RADIUS : ARB_STR_RADIUS
     str = string(x, prec, flags=flags)
     print(io, str)
 end
 
-function show(io::IO, x::ArbComplex{P}; flags::Culong=NO_RADIUS) where {P}
-    prec = digit_precision(P)
+function show(io::IO, x::ArbComplex{P}; midpoint::Bool=false, radius::Bool=false) where {P}
+    prec = midpoint ? digits4bits(P) : digit_precision(P)
+    flags = radius ? ARB_STR_NO_RADIUS : ARB_STR_RADIUS
     str = string(x, prec, flags=flags)
     print(io, str)
 end
