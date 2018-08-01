9066a41485d7617638a1a8d462b888480e163437
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-01 05:15:49 -0400

    Update special2.jl

--- src/float/special2.jl
+++ src/float/special2.jl
@@ -2,10 +2,10 @@ for (A,F) in ((:besselj, :arb_hypgeom_bessel_j), (:bessely, :arb_hypgeom_bessel_
               (:besseli, :arb_hypgeom_bessel_i), (:besselk, :arb_hypgeom_bessel_k))
     @eval begin
         function ($A)(nu::ArbReal{P}, x::ArbReal{P}, prec::Int=P) where {P}
-            z = ArbComplex{P}()
-            ccall(@libarb($F), Cvoid, (Ref{ArbComplex}, Ref{ArbComplex}, Ref{ArbComplex}, Clong),
+            z = ArbReal{P}()
+            ccall(@libarb($F), Cvoid, (Ref{ArbReal}, Ref{ArbReal}, Ref{ArbReal}, Clong),
                                       z, nu, x, prec)
-            return z
+        return z
         end
     end
 end     
@@ -15,22 +15,32 @@ bessely(nu::ArbFloat{P}, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(be
 besseli(nu::ArbFloat{P}, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(besseli(ArbReal{P}(nu), ArbReal{P}(x)))
 besselk(nu::ArbFloat{P}, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(besselk(ArbReal{P}(nu), ArbReal{P}(x)))
 
+besselj(nu::Int, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(besselj(ArbReal{P}(nu), ArbReal{P}(x)))
+bessely(nu::Int, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(bessely(ArbReal{P}(nu), ArbReal{P}(x)))
+besseli(nu::Int, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(besseli(ArbReal{P}(nu), ArbReal{P}(x)))
+besselk(nu::Int, x::ArbFloat{P}, prec::Int=P) where {P} = ArbFloat{P}(besselk(ArbReal{P}(nu), ArbReal{P}(x)))
+
+besselj(nu::Int, x::ArbReal{P}, prec::Int=P) where {P} = besselj(ArbReal{P}(nu), x)
+bessely(nu::Int, x::ArbReal{P}, prec::Int=P) where {P} = bessely(ArbReal{P}(nu), x)
+besseli(nu::Int, x::ArbReal{P}, prec::Int=P) where {P} = besseli(ArbReal{P}(nu), x)
+besselk(nu::Int, x::ArbReal{P}, prec::Int=P) where {P} = besselk(ArbReal{P}(nu), x)
+
 for (A,F) in ((:besselj, :acb_hypgeom_bessel_j), (:bessely, :acb_hypgeom_bessel_y),
               (:besseli, :acb_hypgeom_bessel_i), (:besselk, :acb_hypgeom_bessel_k))
     @eval begin
         function ($A)(nu::ArbComplex{P}, x::ArbComplex{P}, prec::Int=P) where {P}
             z = ArbComplex{P}()
-            ccall(@libarb($F), Cvoid, (Ref{ArbReal}, Ref{ArbReal}, Ref{ArbReal}, Clong),
+            ccall(@libarb($F), Cvoid, (Ref{ArbComplex}, Ref{ArbComplex}, Ref{ArbComplex}, Clong),
                                       z, nu, x, prec)
             return z
         end
     end
 end     
 
-besselj0(x::ArbFloat{P}, prec::Int=P) where {P} = besselj(zero(ArbFloat{P}), x, prec)
-besselj1(x::ArbFloat{P}, prec::Int=P) where {P} = besselj(one(ArbFloat{P}), x, prec)
-bessely0(x::ArbFloat{P}, prec::Int=P) where {P} = bessely(zero(ArbFloat{P}), x, prec)
-bessely1(x::ArbFloat{P}, prec::Int=P) where {P} = bessely(one(ArbFloat{P}), x, prec)
+besselj0(x::ArbFloat{P}, prec::Int=P) where {P} = besselj(zero(ArbReal{P}), ArbReal{P}(x), prec)
+besselj1(x::ArbFloat{P}, prec::Int=P) where {P} = besselj(one(ArbReal{P}), ArbReal{P}(x), prec)
+bessely0(x::ArbFloat{P}, prec::Int=P) where {P} = bessely(zero(ArbReal{P}), ArbReal{P}(x), prec)
+bessely1(x::ArbFloat{P}, prec::Int=P) where {P} = bessely(one(ArbReal{P}), ArbReal{P}(x), prec)
 
 besselj0(x::ArbReal{P}, prec::Int=P) where {P} = besselj(zero(ArbReal{P}), x, prec)
 besselj1(x::ArbReal{P}, prec::Int=P) where {P} = besselj(one(ArbReal{P}), x, prec)
