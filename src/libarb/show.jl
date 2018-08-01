80a90452d403c50c59c0c1e284c53eb460908bd6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-01 16:35:49 -0400

    Update show.jl

--- src/libarb/show.jl
+++ src/libarb/show.jl
@@ -19,11 +19,6 @@ function show(io::IO, x::ArbComplex{P}; midpoint::Bool=false, radius::Bool=false
 end
 
 
-function show(io::IO, x::Mag)
-    str = string(x)
-    print(Base.stdout, str)
-end
-
 function show(x::ArbFloat{P}; midpoint::Bool=false, radius::Bool=false) where {P}
     str = string(x, midpoint=midpoint, radius=radius)
     print(Base.stdout, str)
