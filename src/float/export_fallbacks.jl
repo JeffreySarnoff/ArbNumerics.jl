d7a041a9eba955feaa35c7aab2730d3182f3e7d0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-01 04:19:22 -0400

    Update export_fallbacks.jl

--- src/float/export_fallbacks.jl
+++ src/float/export_fallbacks.jl
@@ -1,7 +1,7 @@
 square(x) = x*x
 cube(x) = x*x*x
-pow(x::T1, y::T2) where {T1,T2} = exp(T1(y) * log(x))
-root(x::T1, y::T2) where {T1,T2} = exp(inv(T1(y)) * log(x))
+pow(x::T1, y::T2) where {T1,T2} = x^T1(y)
+root(x::T1, y::T2) where {T1,T2} = x^inv(T1(y))
 sqrt(x) = root(x, 2.0)
 cbrt(x) = root(x, 3.0)
 rsqrt(x) = sqrt(inv(x))
