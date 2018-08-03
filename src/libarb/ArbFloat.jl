b314cfc8ee8662191476ce3b82c1a72ed457c82b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-02 23:11:04 -0400

    Update ArbFloat.jl

--- src/libarb/ArbFloat.jl
+++ src/libarb/ArbFloat.jl
@@ -127,7 +127,7 @@ for (F,A) in ((:floor, :arf_floor), (:ceil, :arf_ceil))
             ccall(@libarb($A), Cvoid, (Ref{ArbFloat}, Ref{ArbFloat}), z, x)
             return z
         end
-        $F(::Type{T}, x::ArbFloat{P}) where {P} = T($F(x))
+        $F(::Type{T}, x::ArbFloat{P}) where {P, T} = T($F(x))
     end
 end
 
