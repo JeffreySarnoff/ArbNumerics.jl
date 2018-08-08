9fde8510c663930b648f37ed853b2bda41c43b90
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-08 23:03:01 +0100

    Update morearith.jl

--- src/float/morearith.jl
+++ src/float/morearith.jl
@@ -1,7 +1,7 @@
-function square(x::ArbFloat{P}; roundingmode::RoundingMode=RoundNearest) where {P}
+square(x::ArbFloat{P}; roundingmode::RoundingMode=RoundNearest) where {P} = x * x
     z = ArbFloat{P}()
     rounding = match_rounding_mode(roundingmode)
-    ccall(@libarb(arf_mul), Cvoid, (Ref{ArbFloat}, Ref{ArbFloat}, Ref{ArbFloat}, Clong, Cint), z, x, x, P, rounding)
+    ccall(@libarb(arf_mul_rnd_any), Cvoid, (Ref{ArbFloat}, Ref{ArbFloat}, Ref{ArbFloat}, Clong, Cint), z, x, x, P, rounding)
     return z
 end
 
