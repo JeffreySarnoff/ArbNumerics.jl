ae327e3d13d9cc38d245dbc667e1bb6844c3927a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-08 15:28:03 +0100

    Update ArbReal.jl

--- src/libarb/ArbReal.jl
+++ src/libarb/ArbReal.jl
@@ -45,9 +45,9 @@ ArbReal(x, prec::Int) = prec>=MINIMUM_PRECISION ? ArbReal{workingbits(prec)}(x) 
 
 function ArbReal(x::T; bits::Int=0, digits::Int=0, base::Int=iszero(bits) ? 10 : 2) where {T<:Number}
     if base === 10
-        digits = bits4digits(digits)
+        digits = digits > 0 ? bits4digits(digits) : (bits > 0 ? digits4bits(bits) : DEFAULT_PRECISION[1])
     elseif base === 2
-        digits = bits
+        digits = bits > 0 ? bits : (digits > 0 ? digits : DEFAULT_PRECISION[1])
     else
         throw(ErrorException("base expects 2 or 10"))
     end
