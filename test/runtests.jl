39780a5852cf50f70d2f25d7a7491cf0a6215b45
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-08-28 04:57:07 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,5 @@ using ArbNumerics
 using Test
 
 setprecision(BigFloat, 512)
-DIGITS = 78;
-afpi = ArbFloat(pi, bits4digits(DIGITS))
-arpi = ArbReal(pi, bits4digits(DIGITS))
-@test DIGITS == length(string(afpi)) - 1 # (-1 for the decimal point)
-@test DIGITS == length(string(arpi)) - 1 # (-1 for the decimal point)
-
-a = 2*log(ArbFloat(2))
-@test exp(lambertw(a)) == 2
 
 include("arithmetic.jl")
