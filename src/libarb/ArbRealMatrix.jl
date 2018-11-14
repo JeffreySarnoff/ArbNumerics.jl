fc5954ea6eec31992517ea8a0f20267288eadf1f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-11-14 03:16:31 -0500

    Update ArbRealMatrix.jl

--- src/libarb/ArbRealMatrix.jl
+++ src/libarb/ArbRealMatrix.jl
@@ -1,22 +1,4 @@
 # heavily influenced by and mostly of the Arb C interface used in Nemo.jl
-
-linearindex_from_rowcol(nrows::Int, row::Int, col::Int) = row + (col-1)*nrows
-
-linearindex_from_rowcol(m::M, row::Int, col::Int) where {T,M<:AbstractMatrix{T}} =
-    row + (col-1)*size(m)[1]
-
-rowcol_from_linearindex(m::M, linearidx::Int) where {T,M<:AbstractMatrix{T}} =
-    rowcol_from_linearindex(size(m)[1], linearidx)
-
-function rowcol_from_linearindex(nrows::Int, linearidx::Int)
-    row = rem(linearidx, nrows)
-    row = ifelse(row === 0, nrows, row)
-    col = div(linearidx-row, nrows) + 1
-    return row,col
-end
-
-        
-        
         
 #=
 typedef struct
@@ -29,7 +11,10 @@ typedef struct
 arb_mat_struct;
 =#
 
-mutable struct ArbRealMatrix{P} <: AbstractMatrix{ArbReal{P}}
+abstract type AbstractArbMatrix{T} <: AbstractMatrix{T} end
+abstract type AnyArbMatrix{P, T} <: AbstractArbMatrix{T} end
+            
+mutable struct ArbRealMatrix{P} <: AnyArbMatrix{P, ArbReal}
     entries::Ptr{ArbReal{P}}
     nrows::Int
     ncols::Int
