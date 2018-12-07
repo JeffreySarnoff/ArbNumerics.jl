5ace478ca7d877fa618c94e8e36f6e46b3c21e79
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-06 23:41:27 -0500

    Update ArbMatrix.jl

--- src/libarb/ArbMatrix.jl
+++ src/libarb/ArbMatrix.jl
@@ -1,5 +1,2 @@
 abstract type LibArbMatrix{P, T}  <: AbstractMatrix{T} end
 
-abstract type LibArbFloatMatrix{P}   <: LibArbMatrix{P, ArbFloat}    end
-abstract type LibArbRealMatrix{P}    <: LibArbMatrix{P, ArbReal}     end
-abstract type LibArbComplexMatrix{P} <: LibArbMatrix{P, ArbComplex}  end
