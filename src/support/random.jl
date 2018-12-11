3742d18400e26af8f9b9e738b42b3a38d2ba4c15
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-10 22:50:48 -0500

    Update random.jl

--- src/support/random.jl
+++ src/support/random.jl
@@ -1,1 +1,25 @@
+using Random
+using Random: SamplerTrivial, CloseOpen01
+import Random: rand
 
+function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbFloat{P}}
+    oldprec = precision(BigFloat)
+    setprecision(BigFloat, P+9)
+    denom = BigInt(2)^P
+    bigfloat = BigFloat(rand(BigInt(0):denom))
+    bigfloat = bigfloat / BigFloat(denom)
+    result = ArbFloat{P}(bigfloat)
+    setprecision(BigFloat, oldprec)
+    return result
+end
+
+function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbReal{P}}
+    arbfloat = rand(ArbFloat{P})
+    return ArbReal{P}(arbfloat)
+end
+
+function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbComplex{P}}
+    re = ArbReal{P}(rand(ArbFloat{P}))
+    im = ArbReal{P}(rand(ArbFloat{P}))
+    return ArbComplex{P}(re, im)
+end
