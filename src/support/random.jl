e970468618a5cd6b7f37debcb197dcf329eab219
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-12-10 22:55:04 -0500

    Update random.jl

--- src/support/random.jl
+++ src/support/random.jl
@@ -2,7 +2,7 @@ using Random
 using Random: SamplerTrivial, CloseOpen01
 import Random: rand
 
-function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbFloat{P}}
+function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbFloat{P}}
     oldprec = precision(BigFloat)
     setprecision(BigFloat, P+9)
     denom = BigInt(2)^P
@@ -13,13 +13,28 @@ function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbFl
     return result
 end
 
-function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbReal{P}}
+@inline function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{ArbFloat}})
+    P = workingprecision(ArbFloat)
+    return rand(ArbFloat{P})
+end
+
+function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbReal{P}}
     arbfloat = rand(ArbFloat{P})
     return ArbReal{P}(arbfloat)
 end
 
-function rand(:AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbComplex{P}}
+@inline function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{ArbReal}})
+    P = workingprecision(ArbReal)
+    return rand(ArbReal{P})
+end
+
+function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{T}}) where {P, T<:ArbComplex{P}}
     re = ArbReal{P}(rand(ArbFloat{P}))
     im = ArbReal{P}(rand(ArbFloat{P}))
     return ArbComplex{P}(re, im)
+end
+
+@inline function rand(::AbstractRNG, ::SamplerTrivial{CloseOpen01{ArbComplex}})
+    P = workingprecision(ArbFloat)
+    return rand(ArbComplex{P})
 end
