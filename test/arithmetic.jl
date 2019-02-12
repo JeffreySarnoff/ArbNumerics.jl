f07c044dabbd921094bf9a4451ed4a0a2bf0ca34
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-12 01:36:29 -0500

    Update arithmetic.jl

--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -7,6 +7,12 @@ acb = inv(ArbComplex(3))
     @test Float64(arf + 2*arf) == 1.0
     @test Float64(arb + 2*arb) == 1.0
     @test Float64(acb + 2*acb) == 1.0
+    @test Float64(arf - 2*arf) == -1.0
+    @test Float64(arb - 2*arb) == -1.0
+    @test Float64(acb - 2*acb) == -1.0
+    @test Float64(arf * arf / arf) == Float64(arf)
+    @test Float64(arb * arb / arb) == Float64(arf)
+    @test Float64(acb * acb / acb) == Float64(arf)
     @test Float64(arf / arf) == 1.0
     @test Float64(arb / arb) == 1.0
     @test Float64(acb / acb) == 1.0
