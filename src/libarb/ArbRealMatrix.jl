55e3aab8bc393dc4d3911168adfb8be927204a0b
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-25 17:36:25 -0500

    Update ArbRealMatrix.jl

--- src/libarb/ArbRealMatrix.jl
+++ src/libarb/ArbRealMatrix.jl
@@ -291,7 +291,7 @@ end
 
 # matrix multiply
 
-function LinearAlgebra.mul!(z::ArbRealMatrix{P}, x::ArbRealMatrix{P}, y::ArbRealMatrix{P})
+function mul!(z::ArbRealMatrix{P}, x::ArbRealMatrix{P}, y::ArbRealMatrix{P})
     ccall(@libarb(arb_mat_mul), Cvoid, (Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Cint), z, x, y, P)
     return nothing
 end
