f13671733379bc29ca7da5482891fee424dfd8d2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-02-25 09:29:44 -0500

    Update compare.jl

--- src/values/compare.jl
+++ src/values/compare.jl
@@ -95,13 +95,22 @@ end
 
 for F in (:(==), :(!=), :(<), :(<=), :(>=), :(>), :isequal, :isless)
     @eval begin
-        $F(x::ArbFloat{P}, y::T) where {P, T<:Union{Integer, Rational, AbstractFloat}} =
+        $F(x::ArbFloat{P}, y::T) where {P, T<:Union{Integer, AbstractFloat}} =
             $F(promote(x, y)...,)
-        $F(x::T, y::ArbFloat{P}) where {P, T<:Union{Integer, Rational, AbstractFloat}} =
+        $F(x::T, y::ArbFloat{P}) where {P, T<:Union{Integer, AbstractFloat}} =
             $F(promote(x, y)...,)
-        $F(x::ArbReal{P}, y::T) where {P, T<:Union{Integer, Rational, AbstractFloat}} =
+        $F(x::ArbReal{P}, y::T) where {P, T<:Union{Integer, AbstractFloat}} =
             $F(promote(x, y)...,)
-        $F(x::T, y::ArbReal{P}) where {P, T<:Union{Integer, Rational, AbstractFloat}} =
+        $F(x::T, y::ArbReal{P}) where {P, T<:Union{Integer, AbstractFloat}} =
+            $F(promote(x, y)...,)
+
+        $F(x::ArbFloat{P}, y::Rational) where {P} =
+            $F(promote(x, y)...,)
+        $F(x::Rational, y::ArbFloat{P}) where {P} =
+            $F(promote(x, y)...,)
+        $F(x::ArbReal{P}, y::Rational) where {P} =
+            $F(promote(x, y)...,)
+        $F(x::Rational, y::ArbReal{P}) where {P} =
             $F(promote(x, y)...,)
     end
 end
