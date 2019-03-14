00c27072106526ef9c40f91170d7158c755b38be
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-14 05:05:21 -0400

    Create compare.jl

---
+++ test/compare.jl
@@ -0,0 +1,43 @@
+@testset "compare" begin
+
+    @test ArbFloat(2.0) > 1
+    @test ArbFloat(2.0) >= 1
+    @test 1 < ArbFloat(2.0)
+    @test 1 <= ArbFloat(2.0)
+
+    @test ArbFloat(2.0) > 1.0
+    @test 1.0 < ArbFloat(2.0)
+    @test ArbFloat(2.0) >= 1.0
+    @test 1.0 <= ArbFloat(2.0)
+
+    @test ArbFloat(2.0) > ArbFloat(1.0)
+    @test ArbFloat(1.0) < ArbFloat(1.0)
+    @test ArbFloat(2.0) >= ArbFloat(1.0)
+    @test ArbFloat(1.0) <= ArbFloat(1.0)
+
+    @test ArbFloat(2.0) > ArbReal(1.0)
+    @test ArbFloat(1.0) < ArbReal(1.0)
+    @test ArbFloat(2.0) >= ArbFloat(1.0)
+    @test ArbFloat(1.0) <= ArbReal(1.0)
+
+    @test ArbReal(2.0) > ArbFloat(1.0)
+    @test ArbReal(1.0) < ArbFloat(1.0)
+    @test ArbReal(2.0) >= ArbFloat(1.0)
+    @test ArbReal(1.0) <= ArbFloat(1.0)
+
+    @test ArbReal(2.0) > 1
+    @test ArbReal(2.0) >= 1
+    @test 1 < ArbReal(2.0)
+    @test 1 <= ArbReal(2.0)
+
+    @test ArbReal(2.0) > 1.0
+    @test 1.0 < ArbReal(2.0)
+    @test ArbReal(2.0) >= 1.0
+    @test 1.0 <= ArbReal(2.0)
+
+    @test ArbReal(2.0) > ArbReal(1.0)
+    @test ArbReal(1.0) < ArbReal(1.0)
+    @test ArbReal(2.0) >= ArbReal(1.0)
+    @test ArbReal(1.0) <= ArbReal(1.0)
+
+end
