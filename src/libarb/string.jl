5609448e8fd6e51277583813f785854f826cceb6
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-19 18:27:37 -0400

    remove parsing

--- src/libarb/string.jl
+++ src/libarb/string.jl
@@ -103,31 +103,3 @@ function arbstring(x::ArbComplex{P}, maxdigits::Int=digit_precision(P); flags::U
     str = string(rea_str, connection, ima_str, imaginary_unit_str)
     return str
 end
-
-#= from Nemo.jl
-function show(io::IO, x::arb)
-  d = ceil(parent(x).prec * 0.30102999566398119521)
-  cstr = ccall((:arb_get_str, :libarb), Ptr{UInt8}, (Ref{arb}, Int, UInt),
-                                                  x, Int(d), UInt(0))
-  print(io, unsafe_string(cstr))
-  ccall((:flint_free, :libflint), Void, (Ptr{UInt8},), cstr)
-end
-=#
-
-function ArbFloat{P}(s::AbstractString) where {P}
-    return ArbFloat{P}(BigFloat(s))
-end
-
-function ArbReal{P}(s::AbstractString) where {P}
-    return ArbReal{P}(BigFloat(s))
-end
-
-function ArbFloat(s::AbstractString)
-    P = DEFAULT_PRECISION[]
-    return ArbFloat{P}(s)
-end
-
-function ArbReal(s::AbstractString)
-    P = DEFAULT_PRECISION[]
-    return ArbReal{P}(s)
-end
