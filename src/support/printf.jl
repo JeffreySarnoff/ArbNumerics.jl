045f59d39b78a85f7d4142a8b636d76b55b325c3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 03:47:42 -0400

    Update printf.jl

--- src/support/printf.jl
+++ src/support/printf.jl
@@ -3,3 +3,21 @@ Base.Printf.fix_dec(out, d::ArbFloat{P}, flags::String, width::Int, precision::I
 
 Base.Printf.fix_dec(out, d::ArbReal{P}, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_dec(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_dec(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_hex(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_HEX(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_hex(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+
+Base.Printf.ini_HEX(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
