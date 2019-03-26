3b55f48dfb2ff9a37201e68466860be5f85ac5d9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-25 20:26:07 -0400

    Update ArbRealMatrix.jl

--- src/libarb/ArbRealMatrix.jl
+++ src/libarb/ArbRealMatrix.jl
@@ -237,16 +237,6 @@ function mul!(z::ArbRealMatrix{P}, x::ArbRealMatrix{P}, y::ArbRealMatrix{P}) whe
     return nothing
 end
 
-function rmul!(x::ArbRealMatrix{P}, y::ArbRealMatrix{P}) where {P}
-    ccall(@libarb(arb_mat_mul), Cvoid, (Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Cint), x, x, y, P)
-    return nothing
-end
-
-function lmul!(x::ArbRealMatrix{P}, y::ArbRealMatrix{P}) where {P}
-    ccall(@libarb(arb_mat_mul), Cvoid, (Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Cint), y, x, y, P)
-    return nothing
-end
-
 function matmul(x::ArbRealMatrix{P}, y::ArbRealMatrix{P}) where {P}
     z = ArbRealMatrix{P}(rowcount(x), colcount(y))
     ccall(@libarb(arb_mat_mul), Cvoid, (Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Ref{ArbRealMatrix}, Cint), z, x, y, P)
