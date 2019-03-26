f38c3c5c5216dc61ea2f21adf50888fa8cd3db46
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-03-26 03:49:05 -0400

    Update random.jl

--- src/values/random.jl
+++ src/values/random.jl
@@ -16,3 +16,20 @@ function rand(::Type{ArbFloat{P}}, n::Integer) where P
     return result
 end
 
+function rand(rng::AbstractRNG, ::Random.SamplerTrivial{Random.CloseOpen01{ArbReal{P}}},n) where P
+    bfprec = precision(BigFloat)
+    setprecision(BigFloat, P)
+    bfrand = rand(BigFloat,n)
+    result = ArbReal{P}.(bfrand)
+    setprecision(BigFloat, bfprec); println("ok")
+    return result
+end
+
+function rand(::Type{ArbReal{P}}, n::Integer) where P
+    bfprec = precision(BigFloat)
+    setprecision(BigFloat, P)
+    bfrand = rand(BigFloat,n)
+    result = ArbReal{P}.(bfrand)
+    setprecision(BigFloat, bfprec)
+    return result
+end
