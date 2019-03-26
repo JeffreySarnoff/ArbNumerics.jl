30968d128e4bba80c1cc9ddda7e0ed07c185f6e2
autodocs <autodocs>
2019-03-26 12:01:34 +0000

    build based on fa04147

--- dev/search_index.js
+++ dev/search_index.js
@@ -109,7 +109,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Precisions",
     "title": "working precision",
     "category": "section",
-    "text": "ArbNumerics values have an intrinsic precision, which is the number of bits that are evaluated in computation.  Values that are determined by calculation may be displayed at this, their full precision.  If this is the behavior you prefer, start using the package this way:using ArbNumerics\nshow_working_values()"
+    "text": "ArbNumerics values have an intrinsic precision, which is the number of bits that are evaluated in computation.  Values that are determined by calculation may be displayed at this, their full precision.  If this is the behavior you prefer, start using the package this way:using ArbNumerics\nsetextrabits(0)"
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Precisions",
     "title": "enclosed precision",
     "category": "section",
-    "text": "The alternative is to use some of the least significant bits as a \"cushion\" that protects against showing greater precision than is warranted by the accuracy that is assured.  This is particularly important when showing values that are point estimates of an interval that encloses the mathematical result. If you prefer to be shown quantities as point values that best reflect the accuracy available at the end of a computation by letting go of uncertain trailing bits, start using the package this way:using ArbNumerics\nshow_enclosed_values()"
+    "text": "The alternative is to use some of the least significant bits as a \"cushion\" that protects against showing greater precision than is warranted by the accuracy that is assured.  This is particularly important when showing values that are point estimates of an interval that encloses the mathematical result. If you prefer to be shown quantities as point values that best reflect the accuracy available at the end of a computation by letting go of uncertain trailing bits, start using the package this way:using ArbNumerics\nsetextrabits(32)"
 },
 
 {
@@ -125,15 +125,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Precisions",
     "title": "rounded precision",
     "category": "section",
-    "text": "If you specify neither show_working_precision() nor show_enclosed_precision(), then the working precision will exceed the displayed precision by a fixed number of bits.  By default, 24 extra bits are used for the working precision.  This is a settable quantity. If you prefer a different bit differential, you should establish that at the start and you should not alter it midstream:using ArbNumerics\nshow_rounded_values(32)"
+    "text": "If you do specify the number of extrabits to use, then the working precision will exceed the displayed precision by a fixed number of bits (24, the default).setextrabits should be used only at the start of work. Reliabile computations presume you not alter it midstream."
 },
 
 {
-    "location": "precisions/#precision-modality-1",
+    "location": "precisions/#finding-precisions-1",
     "page": "Precisions",
-    "title": "precision modality",
+    "title": "finding precisions",
     "category": "section",
-    "text": "The operative modality is obtainable using bits_rounded(). After show_working_values(), it is 0. After show_rounded_values(nbits), it is nbits.  After show_enclosed_values, it is missing.function precision_modality()\n    nbits = bits_rounded()\n    return if nbits === missing\n               :enclosed\n           elseif iszero(nbits)\n               :working\n           else\n               :rounded\n           end\nend"
+    "text": "The number of displayed bits is given by any of:    precision(ArbFloat), precision(ArbReal), precision(ArbComplex)     All of these are kept in sync.The number of working bits is given by any of:     workingprecision(ArbFloat), workingprecision(ArbReal), workingprecision(ArbComplex)     All of these are kept in sync.The number of extrabits is given by extrabits()."
 },
 
 {
