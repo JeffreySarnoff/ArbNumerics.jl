bdd5e417874b9d582e98932047a73266658025b1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-01 03:58:18 -0400

    Update expmatrix.jl

--- docs/exemplars/expmatrix.jl
+++ docs/exemplars/expmatrix.jl
@@ -18,16 +18,27 @@ results shown are precisely the computed numerical results. Should they differ, 
 Julia should be restarted just before these precisions are altered.  When the precision is being altered
 in the course of iterative refinement, it is the precision of successive values which moves, not their
 bedrock relationship.  Individual values may be constructed more or less precisely than their sources.
+
+Arithmetic over values of differing precision yields a result that adopts the lesser precision.
+
+When the precision is lower than that which permits crisp numerical results, the interval known
+to contain the actual result eclipses the magnitude of that value.  It appears so: `[+/- 12.1]`.
 =#
 
 setextrabits(0)  #  obtain the raw results
-setextrabits(32) #  see less precision to be shown more accuracy
-                 #  there are 24 extra bits at start
+setextrabits(24) #  more accurately, less precision is observed
+#  This is not a means of protection from numerical misadventure.
+#  This may be adaptive accomodation to the partially [un]known.
 
-                                    # use one of these perspectives on counting bits
-setworkingprecision(ArbFloat, 125)  # here use ArbFloat or ArbReal, the setting is shared 
-setprecision(ArbReal, 100) # or set it this way, if that is where the import is greatest
-                           # the most recent `set_` determines the default state of play
+                                    # Use one of these perspectives on bits that count.
+setworkingprecision(ArbFloat, 125)  # This focus is on available computational precision.
+setprecision(ArbReal, 100)          # Or prefer less precision with more accuracy. 
+
+                                    # the most recent `set_` gives the operative precision 
+                                    # for constructing new Arb values using type name only
+                                    # the setting is shared by all of the Arb types
+                                    # and that makes it easier to characterize accuracy
+a = 
 
 #=
 #   During the algorithmic course, one may provide precision to the determination of values.
@@ -47,18 +58,22 @@ workingprecision.((b₂, b₁₀))
 workingprecision(ArbReal(1)) == 128
 workingprecision(ArbFloat(1, bits=1024) + ArbFloat(1, bits=256)) == 256
 
+#>  ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ... ...  <#
+
+const Arb{P} = Union{ArbFloat{P}, ArbReal{P}} where {P}
+
 #=
 #   Obtaining the exponential of a matrix
 #
 #     (a) provision a square matrix
 #     (b) generate a similar matrix of eltype ArbReal 
-#     (c) recieve each of (b) carried from (a)
+#     (c) receive each of (b) carried from (a)
 #     (d) request exp(c) of ArbNumerics                (faster than exp(Matrix{ArbFloat}))
 #     (e) generate a similar matrix given (a)
-#     (f) recieve each of (e) carried from (d)
+#     (f) receive each of (e) carried from (d)
 =#
 
-function expmatrix(::Type{T}, m::Array{F,2}) where {P, T<:Union{ArbFloat{P}, ArbReal{P}}, F<:Real}
+function expmatrix(::Type{T}, m::Array{F,2}) where {P, T<:Arb, F<:Real}
     a = Array{ArbReal,2}(undef, size(m))
     a[:] = m[:]
     z = exp(a)
