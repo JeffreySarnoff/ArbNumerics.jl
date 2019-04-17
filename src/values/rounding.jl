50f91602a049b20cb8d1df242433ea2db9031f72
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-04-17 03:58:20 -0400

    round(ArbFloat|ArbReal, RoundingMode)

--- src/values/rounding.jl
+++ src/values/rounding.jl
@@ -1,26 +1,32 @@
 round(x::ArbFloat{P}, ::RoundingMode{:Up}) where {P} = ceil(x)
 round(x::ArbFloat{P}, ::RoundingMode{:Down}) where {P} = floor(x)
 round(x::ArbFloat{P}, ::RoundingMode{:ToZero}) where {P} = signbit(x) ? ceil(x) : floor(x)
+round(x::ArbFloat{P}, ::RoundingMode{:FromZero}) where {P} = signbit(x) ? floor(x) : ceil(x)
+
+function round(x::ArbFloat{P}, ::RoundingMode{:Nearest}) where {P}
+    absx = abs(x)
+    fl = floor(absx)
+    cl = fl + 1
+    xfl = absx - fl
+    xcl = cl - absx
+    if xfl < xcl
+        copysign(fl, x)
+    elseif xfl > xcl
+        copysign(cl, x)
+    elseif iseven(BigInt(fl))
+        copysign(fl, x)
+    else
+        copysign(cl, x)
+    end
+end
 
 round(x::ArbReal{P}, ::RoundingMode{:Up}) where {P} = ceil(ArbFloat{P}(x))
 round(x::ArbReal{P}, ::RoundingMode{:Down}) where {P} = floor(ArbFloat{P}(x))
 round(x::ArbReal{P}, ::RoundingMode{:ToZero}) where {P} = round(ArbFloat{P}(x), RoundToZero)
+round(x::ArbReal{P}, ::RoundingMode{:FromZero}) where {P} = round(ArbFloat{P}(x), RoundFromZero)
+round(x::ArbReal{P}, ::RoundingMode{:Nearest}) where {P} = round(ArbFloat{P}(x), RoundNearest)
 
-#
-round(x::ArbFloat{P}, ::RoundingMode{::Nearest} where {P} = 
-    z = ArbFloat{P}()
-    rounding = match_rounding_mode(roundingmode)
-    rounddir = ccall(@libarb(arf_set_round), Cint, (Ref{ArbFloat}, Ref{ArbFloat}, Clong, Cint), z, x, P, rounding)
-    return z
-end
-
-function round(x::ArbReal{P}, roundingmode::RoundingMode) where {P}
-    z = ArbReal{P}()
-    rounding = match_rounding_mode(roundingmode)
-    rounddir = ccall(@libarb(arb_set_round), Cint, (Ref{ArbReal}, Ref{ArbReal}, Clong, Cint), z, x, P, rounding)
-    return z
-end
-
+#=
 function round(x::ArbComplex{P}, roundingmode::RoundingMode) where {P}
     z = ArbComplex{P}()
     rounding = match_rounding_mode(roundingmode)
