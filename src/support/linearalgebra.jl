e3257c7ecaff5e9cbf105a5c6314cd4017ad422c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-20 17:00:06 -0400

    Update linearalgebra.jl

--- src/support/linearalgebra.jl
+++ src/support/linearalgebra.jl
@@ -4,9 +4,9 @@ Base.setindex!(v::Array{Complex{ArbFloat{P}},1}, c::ArbComplex{P}, idx::Int64) w
 Base.setindex!(v::Array{Complex{ArbReal{P}},1}, c::ArbComplex{P}, idx::Int64) where {P} =
     setindex!(v, Complex{ArbReal{P}}(real(c), imag(c)), idx)
 
-Base.convert(::ArbFloatMatrix{P}, x::Array{ArbFloat{P}, 2}) = ArbFloatMatrix(x)
-Base.convert(::ArbRealMatrix{P}, x::Array{ArbReal{P}, 2}) = ArbRealMatrix(x)
-Base.convert(::ArbComplexMatrix{P}, x::Array{ArbComplex{P}, 2}) = ArbComplexMatrix(x)
+Base.convert(::ArbFloatMatrix{P}, x::Array{ArbFloat{P}, 2}) where {P} = ArbFloatMatrix(x)
+Base.convert(::ArbRealMatrix{P}, x::Array{ArbReal{P}, 2})  where {P} = ArbRealMatrix(x)
+Base.convert(::ArbComplexMatrix{P}, x::Array{ArbComplex{P}, 2}) where {P} = ArbComplexMatrix(x)
 
 #svdvals(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
 #svdvals(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
