d24561d5953e3bbaca6e680fa62d2d5e070b6c4a
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-20 02:52:30 -0400

    Update minprec.jl

--- src/support/minprec.jl
+++ src/support/minprec.jl
@@ -1,7 +1,7 @@
 # protect minimum precision
 minprecerror() = throw(DomainError("minimum precision is 24 bits"))
 
-for T in (:Float32, :Float64, :Int32, :Int64, :Int128}
+for T in (:Float32, :Float64, :Int16, :Int32, :Int64, :Int128, :BigFloat)
   for N in collect(0:23)
     @eval ArbFloat{$N}(x::$T) = minprecerror()
     @eval ArbReal{$N}(x::$T) = minprecerror()
