46ad577abf02563352b8397122bac179bcc03fe5
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-20 16:50:09 -0400

    setindex!

--- src/support/linearalgebra.jl
+++ src/support/linearalgebra.jl
@@ -1,8 +1,14 @@
-svdvals(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
-svdvals(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+Base.setindex!(v::Array{Complex{ArbFloat{P}},1}, c::ArbComplex{P}, idx::Int64) where {P} =
+    setindex!(v, Complex{ArbFloat{P}}(ArbFloat{P}(real(c)), ArbFloat{P}(imag(c))), idx)
 
-eigvals(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
-eigvals(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
-LinearAlgebra.eigvecs(x::Array{ArbFloat{N},M}) where {N,M} = throw(ErrorException("not implemented"))
-LinearAlgebra.eigvecs(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
-LinearAlgebra.eigvecs(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+Base.setindex!(v::Array{Complex{ArbReal{P}},1}, c::ArbComplex{P}, idx::Int64) where {P} =
+    setindex!(v, Complex{ArbReal{P}}(real(c), imag(c)), idx)
+
+#svdvals(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+#svdvals(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+
+#eigvals(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+#eigvals(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+#LinearAlgebra.eigvecs(x::Array{ArbFloat{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+#LinearAlgebra.eigvecs(x::Array{ArbReal{N},M}) where {N,M} = throw(ErrorException("not implemented"))
+#LinearAlgebra.eigvecs(x::Array{ArbComplex{N},M}) where {N,M} = throw(ErrorException("not implemented"))
