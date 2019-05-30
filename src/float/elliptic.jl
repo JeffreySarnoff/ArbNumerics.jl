97d764db1889fbbc0c3c18b653e045b62a2811b1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-05-30 02:06:10 -0400

    Update elliptic.jl

--- src/float/elliptic.jl
+++ src/float/elliptic.jl
@@ -242,5 +242,18 @@ end
 
 
 
+#= 
 
+void acb_elliptic_p(acb_t res, const acb_t z, const acb_t tau, slong prec)¶
+Computes Weierstrass’s elliptic function
+
+void acb_elliptic_inv_p(acb_t res, const acb_t z, const acb_t tau, slong prec)¶
+Computes the inverse of the Weierstrass elliptic function
+
+void acb_elliptic_zeta(acb_t res, const acb_t z, const acb_t tau, slong prec)¶
+Computes the Weierstrass zeta function
+
+void acb_elliptic_sigma(acb_t res, const acb_t z, const acb_t tau, slong prec)¶
+Computes the Weierstrass sigma function
+=#
 
