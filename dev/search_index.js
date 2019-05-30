2cf17ca9472098488776ce9cf9ab376b36d71720
autodocs <autodocs>
2019-05-30 05:07:17 +0000

    build based on 06e33e8

--- dev/search_index.js
+++ dev/search_index.js
@@ -301,7 +301,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Elliptic Functions",
     "title": "Complete Elliptic Functions",
     "category": "section",
-    "text": "elliptick(m)\n|       the complete elliptic integral of the first kind            |    |:––––––––––––––––––––––––––––––––-:|    |    <img src=\"assets/elliptick.png\" alt=\"elliptick\" width=\"500\">     |\nelliptice(m)\n|       the complete elliptic integral of the second kind           |    |:––––––––––––––––––––––––––––––––-:|    |    <img src=\"assets/elliptice.png\" alt=\"elliptice\" width=\"500\">     |\nellipticpi(n, m)\n|       the complete elliptic integral of the third kind            |    |:––––––––––––––––––––––––––––––––-:|    |    <img src=\"assets/ellipticpi.png\" alt=\"ellipticpi\" width=\"500\">     |"
+    "text": "elliptick(m)\n|       the complete elliptic integral of the first kind            |    |:––––––––––––––––––––––––––––––––-:|    |  <img src=\"assets/elliptick.png\" alt=\"elliptick\" width=\"500\">     |\nelliptice(m)\n|       the complete elliptic integral of the second kind           |    |:––––––––––––––––––––––––––––––––-:|    |  <img src=\"assets/elliptice.png\" alt=\"elliptice\" width=\"500\">     |\nellipticpi(n, m)\n|       the complete elliptic integral of the third kind            |    |:––––––––––––––––––––––––––––––––-:|    |   <img src=\"assets/ellipticpi.png\" alt=\"ellipticpi\" width=\"600\">  |"
 },
 
 {
@@ -309,7 +309,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Elliptic Functions",
     "title": "Incomplete Elliptic Functions",
     "category": "section",
-    "text": "ellipticf(phi, m)\n|       the incomplete elliptic integral of the first kind           |    |:––––––––––––––––––––––––––––––––-:|    |    <img src=\"assets/ellipticf.png\" alt=\"ellipticf\" width=\"500\">     |"
+    "text": "ellipticf(phi, m)\n|       the incomplete elliptic integral of the first kind           |    |:–––––––––––––––––––––––––––––––––:|    |   <img src=\"assets/ellipticf.png\" alt=\"ellipticf\" width=\"500\">     |"
 },
 
 {
