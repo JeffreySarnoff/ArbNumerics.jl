252c961daa6e1e2b04207bd7c442d73cdbb844b0
autodocs <autodocs>
2019-05-31 05:50:13 +0000

    build based on 8990866

--- dev/search_index.js
+++ dev/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "The Types",
     "title": "ArbReal",
     "category": "section",
-    "text": "An ArbReal value is implemented in the underlying C library as an ArbFloat midpoint and a value enclosing radius.<div><img align=\"center\" src=\"./assets/midrad.jpg\" alt=\"midrad\" width=\"500\"></img></div>"
+    "text": "An ArbReal value is implemented in the underlying C library as an ArbFloat midpoint and a value enclosing radius.<img align=\"right\" src=\"./assets/midrad.jpg\" alt=\"midrad\" width=\"500\"></img>"
 },
 
 {
