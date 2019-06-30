e901263ed043703145cf73b764ad44825d6b37d7
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-06-30 07:47:35 -0400

    Update ArbNumerics.jl

--- src/ArbNumerics.jl
+++ src/ArbNumerics.jl
@@ -58,7 +58,7 @@ export ArbNumber,
        airyai, airyaiprime, airybi, airybiprime,
        besselj, besselj0, besselj1, bessely, bessely0, bessely1, besseli, besselk,
        hypergeometric_0F1, hypergeometric_1F1, hypergeometric_2F1,
-       hypergeometric_0F1_regularized, hypergeometric_1F1_regularized, hypergeometric_2F1_regularized,
+       regular_hypergeometric_0F1, regular_hypergeometric_1F1, regular_hypergeometric_2F1,
        F₀₁,  F₁₁, F₂₁, regularF₀₁,  regularF₁₁, regularF₂₁,
        elliptic_k, elliptic_e, elliptic_pi, elliptic_f,
        elliptic_k2, elliptic_e2, elliptic_pi2, elliptic_f2, # modulus^2 
