564c1cb57d564a5d9906081ec8bcfcfda54b04bc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-07-03 22:56:25 -0400

    Update constructors.jl

--- src/values/constructors.jl
+++ src/values/constructors.jl
@@ -151,7 +151,7 @@ ArbComplex{P}(x::BigFloat) where {P} = ArbComplex{P}(ArbReal{P}(x))
 ArbComplex{P}(x::T) where {P,T<:Real} = ArbComplex{P}(BigFloat(x))
 #ArbComplex(x::T) where {T<:Real} = ArbComplex{workingprecision(ArbComplex)}(BigFloat(x))
 ArbComplex{P}(x::T, y::T) where {P,T<:Real} = ArbComplex{P}(BigFloat(x), BigFloat(y))
-ArbComplex(x::T, y::T) where {T<:Real} = ArbComplex{workingprecision(ArbComplex)}(BigFloat(x), BigFloat(y))
+#ArbComplex(x::T, y::T) where {T<:Real} = ArbComplex{workingprecision(ArbComplex)}(BigFloat(x), BigFloat(y))
 ArbComplex{P}(x::T) where {P,T<:Complex} = ArbComplex{P}(BigFloat(real(x)), BigFloat(imag(x)))
 ArbComplex(x::T) where {T<:Complex} = ArbComplex{workingprecision(ArbComplex)}(BigFloat(real(x)), BigFloat(imag(x)))
 
