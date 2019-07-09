0c4aedc7b8f5f1e0184ef5a3cfbb7eafbb5ebba7
autodocs <autodocs>
2019-07-09 20:26:16 +0000

    build based on 70dae30

--- dev/search_index.js
+++ dev/search_index.js
@@ -425,17 +425,17 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "libarb_differences/#LibArb\'s-values-do-not-remember-their-own-precision-1",
+    "location": "libarb_differences/#LibArb-values-do-not-own-their-precision-1",
     "page": "LibArb Differences",
-    "title": "LibArb\'s values do not remember their own precision",
+    "title": "LibArb values do not own their precision",
     "category": "section",
     "text": "The input values have no memory of the precision that was used to create them. An input value could be, say,    [1.0 +/- 0.25], regardless of whether the precision used to compute it was 2 bits, 100 bits, or 1000000 bits.Just think of a ball as a pair of rational numbers, not in terms of precision. The precision p for the new operation specifies (in almost all functions) that the midpoint of the output ball is to be rounded to at most p bits; if p = 100 then the output will be rounded to at most 100 bits even if the inputs have 1000000 bits.– Fredrik Johansson on nemo-dev, 2018-Dec-05"
 },
 
 {
-    "location": "libarb_differences/#ArbNumerics\'s-values-remember-their-own-precision-1",
+    "location": "libarb_differences/#ArbNumerics-values-own-their-own-precision-1",
     "page": "LibArb Differences",
-    "title": "ArbNumerics\'s values remember their own precision",
+    "title": "ArbNumerics values own their own precision",
     "category": "section",
     "text": "works properly with mixed precision arithmetic\nworks as intended with precision reduction\nmaintains value integrity with precision expansion"
 },
