3aed73420d9337c93ced720fd00705d34d62967e
autodocs <autodocs>
2019-07-10 04:03:23 +0000

    build based on 39adfd0

--- dev/search_index.js
+++ dev/search_index.js
@@ -429,7 +429,7 @@ var documenterSearchIndex = {"docs": [
     "page": "LibArb Differences",
     "title": "LibArb values do not own their precision",
     "category": "section",
-    "text": "The input values have no memory of the precision that was used to create them. An input value could be, say,    [1.0 +/- 0.25], regardless of whether the precision used to compute it was 2 bits, 100 bits, or 1000000 bits.Just think of a ball as a pair of rational numbers, not in terms of precision. The precision p for the new operation specifies (in almost all functions) that the midpoint of the output ball is to be rounded to at most p bits; if p = 100 then the output will be rounded to at most 100 bits even if the inputs have 1000000 bits.– Fredrik Johansson on nemo-dev, 2018-Dec-05"
+    "text": "The input values have no memory of the precision that was used to create them. An input value could be,    [1.0 +/- 0.25], irrespective of the precision used to compute it – was it 2 bits, 100 or 1_000 bits.Just think of a ball as a pair of rational numbers, not in terms of precision. The precision p for the new operation specifies ... that the midpoint of the output ball is to be rounded to at most p bits; if p = 100 then the output will be rounded to at most 100 bits even if the inputs have 1000000 bits.– Fredrik Johansson on nemo-dev, 2018-Dec-05"
 },
 
 {
