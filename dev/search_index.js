22ad85110f66a210fd604f1b0d2e995d6dc841bf
autodocs <autodocs>
2019-07-10 03:03:28 +0000

    build based on 783da6e

--- dev/search_index.js
+++ dev/search_index.js
@@ -129,6 +129,38 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "precisions/#operating-precision-1",
+    "page": "Precisions",
+    "title": "operating precision",
+    "category": "section",
+    "text": "For some numerical work, it is necessary that all significant bits/digits of each value computed be visible.  This is especially true with algorithmic development, precision-sensitive analysis, and delivery of numerical assurances.This mode of use obtains with an initial directive: setextrabits(0)."
+},
+
+{
+    "location": "precisions/#perceptual-precision-1",
+    "page": "Precisions",
+    "title": "perceptual precision",
+    "category": "section",
+    "text": "For some numerical work, it is preferred that all values are displayed as accurately as circumstance may allow, and without any non-informing or potentially misleading trailing digits.  This is certainly true when computation is designed to inform decisions, where the purpose of calculation is distill from complexity some coherence, or when the use of computed results has substantive consequences.This mode of use obtains by default, and may be made more or less coarse with an initial directive: setextrabits(_nbits_) where nbits is whatever you choose.  The default is 24 bits, and unless it is set to zero (see operating precision), that is the lowest recommended value."
+},
+
+{
+    "location": "precisions/#ArbFloat-1",
+    "page": "Precisions",
+    "title": "ArbFloat",
+    "category": "section",
+    "text": "It is reasonable to use settings of {32, 48, or 64} with ArbFloats.With ArbFloat, the extra bits serve to pad away some numerical noise that otherwise could accumulate as uncertainty or opacity. When using ArbFloats in an ill-conditioned calculation, consider raising this setting – you will see the same number of digits, the internal precision will have increased."
+},
+
+{
+    "location": "precisions/#ArbReal,-ArbComplex-1",
+    "page": "Precisions",
+    "title": "ArbReal, ArbComplex",
+    "category": "section",
+    "text": "It is reasonable to use settings of {64, 96, 128} with ArbReal, and {64, 128, 196} with ArbComplex.With ArbReal and ArbComplex, the extra bits serve to extend the proper intelligebility of the midpoint within an enclosure. Enclosures are given as a pair: midpoint, radius, called a ball. When the radius is quite small relative to the position of the midpoint, then the midpoint itself is a better reflection of the enclosed true or the theoretical best value.  Increasing the extrabits serves to push the radius farther \"down\" along the midpoint (as it were), which, at least temporarily, makes it more resiliant to gross widening."
+},
+
+{
     "location": "precisions/#working-precision-1",
     "page": "Precisions",
     "title": "working precision",
