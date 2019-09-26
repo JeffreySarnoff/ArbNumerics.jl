626dabe1364e14d73aefb7c154e35b393490d411
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-09-26 12:05:13 -0400

    support v1.0+

--- src/support/printf.jl
+++ src/support/printf.jl
@@ -1,23 +1,38 @@
-Base.Printf.fix_dec(out, d::ArbFloat{P}, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
-    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+import Printf: ini_dec, fix_dec, ini_hex, ini_HEX
 
-Base.Printf.fix_dec(out, d::ArbReal{P}, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+if VERSION < v"1.1"
+using Base.Grisu: DIGITSs
+fix_dec(out, d::ArbFloat{P}, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+fix_dec(out, d::ArbReal{P}, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_dec(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_dec(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_hex(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_HEX(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_hex(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+ini_HEX(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, DIGITSs[Threads.threadid()])
+else
+fix_dec(out, d::ArbFloat{P}, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_dec(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+fix_dec(out, d::ArbReal{P}, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_dec(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+ini_dec(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_hex(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+ini_dec(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_HEX(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+ini_hex(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_hex(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+ini_HEX(out, d::ArbFloat{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
-
-Base.Printf.ini_HEX(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+ini_hex(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
     Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+ini_HEX(out, d::ArbReal{P}, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) where P = 
+    Base.Printf.bigfloat_printf(out, BigFloat(d), flags, width, precision, c, digits)
+end    
