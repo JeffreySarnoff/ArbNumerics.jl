fa65c29ff221e9551310b396a4d32868ebf5ebed
Isaia Nisoli <nisoli@im.ufrj.br>
2019-10-22 09:45:21 -0300

    Scrapped the whole ArbComplexVector, memory allocation and deallocation is now explicit in DFT and InverseDFT, input is an Array{ArbComplex,1}, output is an Array{ArbComplex{P},1} where P is the ArbComplex working precision

--- src/libarb/ArbFFT.jl
+++ src/libarb/ArbFFT.jl
@@ -1,114 +1,85 @@
-abstract type AbstractArbVector{P, T}  <: AbstractVector{T} end
-
 export DFT, InverseDFT
 
-mutable struct ArbComplexVector{P} <: AbstractArbVector{P, ArbComplex}
-    length::Int
-    data::Ptr{ArbComplex{P}}
-
-    function ArbComplexVector{P}(length::Int) where {P}
-        z = new{P}() 
-        acb_vec_init(z, length)
-        finalizer(acb_vec_clear, z)
-        return z
-    end
-end
-
-ArbComplexVector(x::ArbComplexVector{P}) where {P} = x
-ArbComplexVector{P}(x::ArbComplexVector{P}) where {P} = x
-
-@inline function acb_vec_clear(x::ArbComplexVector{P}) where {P}
-    ccall(@libarb(_acb_vec_clear), Cvoid, (Ref{ArbComplex{P}}, Cint ), x.data, x.length)
-end
-
-@inline function acb_vec_init(x::ArbComplexVector{P}, length::I) where {P, I<:Signed}
-    x.data = ccall(@libarb(_acb_vec_init), Ptr{ArbComplex{P}}, (Int32,), length)
-    x.length = length
-end
-
-@inline function ArbComplexVector(length::Int)
-    P = workingprecision(ArbComplex)
-	return ArbComplexVector{P}(length)
-end
-
-
-function ArbComplexVector{P}(fpm::Array{ArbComplex{P},1}) where {P}
-    length = size(fpm)[1]
-    arv = ArbComplexVector{P}(length)
+function DFT(x::Array{ArbComplex{P},1}) where {P}
+    # to guarantee safe bounds
+    length = size(x)[1]
+    
+    # we transfer the data into a C acb vector
+    
+    #(ALLOC x_c) Allocate the memory in C space
+    x_c = ccall(@libarb(_acb_vec_init), Ptr{ArbComplex{P}}, (Int32,), length)
+    #
+    
+    # copy x_c
     @inbounds for i in 1:length
-                arv[i]=fpm[i]
+                unsafe_store!(x_c, x[i], i)
               end
-    return arv
-end
-
-function ArbComplexVector(fpm::Array{ArbComplex{P},1}) where {P}
-    length = size(fpm)[1]
-    arv = ArbComplexVector{P}(length)
+    #
+    
+    #(ALLOC transf_C)
+    transf_c = ccall(@libarb(_acb_vec_init), Ptr{ArbComplex{P}}, (Int32,), length)
+    #
+    
+    # we call the acb_dft 
+    # Signature: void acb_dft(acb_ptr w, acb_srcptr v, slong n, slong prec)
+    ccall(@libarb(acb_dft), Cvoid, (Ref{ArbComplex{P}}, Ref{ArbComplex{P}}, Cint, Cint ), transf_c, x_c, length, P)
+    
+    #(DEALLOC x_c)
+    ccall(@libarb(_acb_vec_clear), Cvoid, (Ref{ArbComplex{P}}, Cint ), x_c, length)
+    #
+    
+    transf = zeros(ArbComplex{P}, length)
     @inbounds for i in 1:length
-                arv[i]=fpm[i]
+                transf[i] = unsafe_load(transf_c, i)
               end
-    return arv
+    
+    #(DEALLOC transf_c)
+    ccall(@libarb(_acb_vec_clear), Cvoid, (Ref{ArbComplex{P}}, Cint ), transf_c, length)
+    #
+    
+    return transf
 end
 
-@inline function ArbComplexVector(fpm::Array{ArbComplex,1})
-    P = workingprecision(ArbComplex)
-    length = size(fpm)[1]
-    arv = ArbComplexVector{P}(length)
+DFT(x::Array{ArbComplex,1}) = DFT(Array{ArbComplex{workingprecision(ArbComplex)}, 1}(x))
+
+function InverseDFT(x::Array{ArbComplex{P},1}) where {P}
+    # to guarantee safe bounds
+    length = size(x)[1]
+    
+    # we transfer the data into a C acb vector
+    
+    #(ALLOC x_c) Allocate the memory in C space
+    x_c = ccall(@libarb(_acb_vec_init), Ptr{ArbComplex{P}}, (Int32,), length)
+    #
+    
+    # copy x_c
     @inbounds for i in 1:length
-                arv[i]=fpm[i]
+                unsafe_store!(x_c, x[i], i)
               end
-    return arv
+    #
+    
+    #(ALLOC transf_C)
+    transf_c = ccall(@libarb(_acb_vec_init), Ptr{ArbComplex{P}}, (Int32,), length)
+    #
+    
+    # we call the acb_dft 
+    # Signature: void acb_dft(acb_ptr w, acb_srcptr v, slong n, slong prec)
+    ccall(@libarb(acb_dft_inverse), Cvoid, (Ref{ArbComplex{P}}, Ref{ArbComplex{P}}, Cint, Cint ), transf_c, x_c, length, P)
+    
+    #(DEALLOC x_c)
+    ccall(@libarb(_acb_vec_clear), Cvoid, (Ref{ArbComplex{P}}, Cint ), x_c, length)
+    #
+    
+    transf = zeros(ArbComplex{P}, length)
+    @inbounds for i in 1:length
+                transf[i] = unsafe_load(transf_c, i)
+              end
+    
+    #(DEALLOC transf_c)
+    ccall(@libarb(_acb_vec_clear), Cvoid, (Ref{ArbComplex{P}}, Cint ), transf_c, length)
+    #
+    
+    return transf
 end
 
-
-function Base.getindex(V::ArbComplexVector{P}, i) where {P}
-    @assert(i>0 && i<=V.length)
-    return unsafe_load(V.data, i)
-end
-
-function Base.setindex!(V::ArbComplexVector{P}, x::ArbComplex{P}, i) where {P}
-    @assert(i>0 && i<=V.length)
-    return unsafe_store!(V.data, x, i)
-end
-
-@inline Base.size(x::ArbComplexVector{P}) where {P} = (x.length,)
-
-function DFT(x::ArbComplexVector{P}) where {P}
-    length = x.length
-    transf = ArbComplexVector{P}(length)
-    # we call the acb_dft void acb_dft(acb_ptr w, acb_srcptr v, slong n, slong prec)
-    ccall(@libarb(acb_dft), Cvoid, (Ref{ArbComplex{P}}, Ref{ArbComplex{P}}, Cint, Cint ), transf.data, x.data, x.length, P)
-    return Array{ArbComplex{P},1}(transf)
-end
-
-function InverseDFT(x::ArbComplexVector{P}) where {P}
-    length = x.length
-    transf = ArbComplexVector{P}(length)
-    # we call the acb_dft void acb_dft_inverse(acb_ptr w, acb_srcptr v, slong n, slong prec)
-    ccall(@libarb(acb_dft_inverse), Cvoid, (Ref{ArbComplex{P}}, Ref{ArbComplex{P}}, Cint, Cint ), transf.data, x.data, x.length, P)
-    return Array{ArbComplex{P},1}(transf)
-end
-
-
-DFT(x::Array{ArbComplex{P},1}) where {P} = DFT(ArbComplexVector{P}(x))
-DFT(x::Array{ArbComplex,1}) = DFT(ArbComplexVector(x))
-
-InverseDFT(x::Array{ArbComplex{P},1}) where {P} = InverseDFT(ArbComplexVector{P}(x))
-InverseDFT(x::Array{ArbComplex,1}) = InverseDFT(ArbComplexVector(x))
-
- 
-@inline function radius(V::ArbComplexVector{P}) where {P}
-    return ArbComplexVector([radius(x) for x in V])
-end
-
-@inline function midpoint(V::ArbComplexVector{P}) where {P}
-    return ArbComplexVector([midpoint(x) for x in V])
-end
-
-@inline function real(V::ArbComplexVector{P}) where {P}
-    return [real(x) for x in V]
-end
-
-@inline function imag(V::ArbComplexVector{P}) where {P}
-    return [imag(x) for x in V]
-end
+InverseDFT(x::Array{ArbComplex,1}) = InverseDFT(Array{ArbComplex{workingprecision(ArbComplex)}, 1}(x))
