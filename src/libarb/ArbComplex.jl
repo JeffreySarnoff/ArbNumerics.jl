893837bf005a72b582efaa78386b1ccf417dc9c2
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2019-10-23 23:14:50 -0400

    remove ambiguities

--- src/libarb/ArbComplex.jl
+++ src/libarb/ArbComplex.jl
@@ -345,10 +345,10 @@ trunc(x::ArbComplex{P}) where {P} = ArbComplex{P}(trunc(real(x)), trunc(imag(x))
 trunc(::Type{T}, x::ArbComplex{P}) where {P, T<:Integer} = T(trunc(real(x))), T(trunc(imag(x)))
 
 floor(x::ArbComplex{P}) where {P} = ArbComplex{P}(florr(real(x)), floor(imag(x)))
-floor(::Type{T}, x::ArbComplex{P}) where {P, T} = T(floor(real(x))), T(floor(imag(x)))
+floor(::Type{T}, x::ArbComplex{P}) where {P, T<:Integer} = T(floor(real(x))), T(floor(imag(x)))
 
 ceil(x::ArbComplex{P}) where {P} = ArbComplex{P}(ceil(real(x)), ceil(imag(x)))
-ceil(::Type{T}, x::ArbComplex{P}) where {P, T} = T(ceil(real(x))), T(ceil(imag(x)))
+ceil(::Type{T}, x::ArbComplex{P}) where {P, T<:Integer} = T(ceil(real(x))), T(ceil(imag(x)))
 
 function modf(x::ArbComplex{P}) where {P}
     repart = modf(real(x))
